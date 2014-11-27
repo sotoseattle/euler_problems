ebc888fe881ed2dc9438249276a7c96efe421309
Javier Soto <sotoseattle@gmail.com>
2014-11-27 11:57:12 -0800

    problem 40

---
+++ prb_40/prb_40.rb
@@ -0,0 +1,14 @@
+# An irrational decimal fraction is created by concatenating the positive integers:
+# 0.123456789101112131415161718192021...
+
+# It can be seen that the 12th digit of the fractional part is 1.
+
+# If dn represents the nth digit of the fractional part, find the value of the
+# following expression.
+# d1 × d10 × d100 × d1000 × d10000 × d100000 × d1000000
+
+def d(n)
+  (1..n).to_a.join[n-1].to_i
+end
+
+p [*(0..6)].map { |e| d(10**e) }.reduce(:*)
